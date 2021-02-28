e91da5103d6f20cd44ce2341b3901e2c74fbb7fb
Ubuntu User <max@max.com>
2021-02-28 00:16:26 +0000

    communicatin mediator

--- executer.py
+++ executer.py
@@ -1,8 +1,10 @@
 import rospy
 from geometry_msgs.msg import Twist
 from kobuki_msgs.msg import BumperEvent
+from multiprocessing import Process
 from math import radians
 from random import random
+from mediator import Mediator
 import pyttsx3
 
 
@@ -10,31 +12,49 @@ class Executer():
 
     
     def __init__(self, queue):
-        self.queue = queue
+        self.receiver = Queue()
+        self.sender = Queue()
+        self.mediator = Mediator(self.receiver, self.sender)
+        self.start()
         rospy.init_node('executer', anonymous=False)
         rospy.on_shutdown(self.shutdown)
         self.rate = rospy.Rate(5)
         self.commander = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-        #self.bumper = rospy.Subscriber('mobile_base/events/bumper', BumperEvent, self.processBump)
+        self.bumper = rospy.Subscriber('mobile_base/events/bumper', BumperEvent, self.processBump)
 
-        
+
+    def processBump(self, data):
+        if (data.state == BumperEvent.PRESSED):
+            package = {
+                'type': 'bumper',
+            }
+            if data.bumper == 1:
+                package['value'] = 'front'
+            elif data.bumper == 0:
+                package['value'] = 'left'
+            elif data.bumper == 2:
+                package['value'] = 'right'
+            sender.put(package)
+        else:
+            rospy.loginfo("Bumper Event")
+            rospy.loginfo(data.bumper)
+
+
     def run(self):
 
         twist = Twist()
 
         while not rospy.is_shutdown():
 
-            if not self.queue.empty():
-                command = self.queue.get()
-                print(command)
+            if not self.receiver.empty():
+                command = self.receiver.get()
                 twist = Twist()
-                if command[0] == 0:
-                    print('going')
-                    twist.linear.x = command[1]
-                if command[0] == 1:
+                if command['type'] == 'straight':
+                    twist.linear.x = command['value']
+                if command['type'] == 'turn':
                     twist.linear.x = 0
-                    twist.angular.z = radians(command[1])
-                print(twist)
+                    twist.angular.z = radians(command['value'])
+                print(command)
 
             self.commander.publish(twist)
             self.rate.sleep()
---
+++ mediator.py
@@ -0,0 +1,36 @@
+import network
+import json
+from multiprocessing import Process
+
+
+class Mediator(Process):
+
+
+    def __init__(self, receiveQueue, sendQueue, ip=None):
+        self.daemon = True
+        super().__init__()
+        self.receiver = receiveQueue
+        self.sender = sendQueue
+        if ip:
+            network.call(sys.argv[1], whenHearCall=self.receive)
+        else:
+            network.wait(whenHearCall=self.receive)
+
+
+    def receive(self, obj):
+        try:
+            data = json.loads(obj)
+        except Exception:
+            print('Failed to parse json')
+        else:
+            print('Received:,' obj)
+            self.receiver.put(data)
+        
+
+    def run(self):
+        while network.isConnected():
+            if self.sender.empty():
+                data = self.empty.get()
+                network.say(json.dumps(data))
+            
+        
